f85774e96264af50643e398b7d6e69290e001d81
kurocafe <aiue7298@gmail.com>
2024-11-28 18:46:33 +0900

    update: test()

--- func/message_response/response.py
+++ func/message_response/response.py
@@ -1,10 +1,15 @@
 import ollama
 import traceback
 from ctypes import *
+from langchain_community.embeddings import OllamaEmbeddings
+from llama_index.llms.ollama import Ollama
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.core.node_parser import SentenceSplitter
+import pymupdf4llm
 
 model_file='''
-FROM ./Llama-3-ELYZA-JP-8B-q4_k_m.gguf
-SYSTEM あなたは優しいおしとやかな女性です。描写はいらないです。セリフだけ送ってください。
+FROM elyza:jp8b
+SYSTEM あなたは大学の教授です。学生の研究のアドバイスをします
 TEMPLATE """{{ if .System }}<|start_header_id|>system<|end_header_id|>
 
 {{ .System }}<|eot_id|>{{ end }}{{ if .Prompt }}<|start_header_id|>user<|end_header_id|>
@@ -18,12 +23,39 @@ PARAMETER stop "<|eot_id|>"
 PARAMETER stop "<|reserved_special_token"
 '''
 
+def test():
+    llm = Ollama(model='llama3_rag', request_timeout=30.0)
+    embed_model = OllamaEmbeddings(model="mxbai-embed-large")
+    llama_reader = pymupdf4llm.LlamaMarkdownReader()
+    llama_docs = llama_reader.load_data("/mnt/data1/home/nakaura/VSCode/llama/sota_server/func/message_response/pdf/MuSE.pdf")
+    # print(f"LlamaIndexドキュメントの数: {len(llama_docs)}")
+    # print(f"最初のドキュメントの内容: {llama_docs[0].text[:500]}")
+    # reader = SimpleDirectoryReader(input_files=["/mnt/data1/home/nakaura/VSCode/llama/sota_server/func/message_response/pdf/MuSE.pdf"])
+    # data = reader.load_data()
+    index = VectorStoreIndex.from_documents(llama_docs, embed_model=embed_model, transformations=[SentenceSplitter(chunk_size=256)])
+    # index = VectorStoreIndex.from_documents(llama_docs, embed_model=embed_model)
+    query_engine = index.as_query_engine(llm=llm, streaming=False, similarity_top_k=5)
+    response = query_engine.query("私は感情分析の研究をしたいと考えています。使用すべきデータセットを教えてください。")
+    print(response)
+    
+    
+import fitz
+
+def join():
+    doc1 = fitz.open("/mnt/data1/home/nakaura/VSCode/llama/sota_server/func/message_response/pdf/MuSE.pdf")
+    
+    doc2 = fitz.open("/mnt/data1/home/nakaura/VSCode/llama/sota_server/func/message_response/pdf/IEMOCAP.pdf")
+    
+    doc1.insert_pdf(doc2)
+    
+    doc1.save("/mnt/data1/home/nakaura/VSCode/llama/sota_server/func/message_response/pdf/joined.pdf")
+
 
 def load_model(model_file):
     print('OLLAMA')
     # createしたらどっかに保存されるので2回目以降は作らなくてもいい
     try: 
-        ollama.create(model='llama3_soft', modelfile=model_file)
+        ollama.create(model='llama3_rag', modelfile=model_file)
     except Exception as e:
         print(f"エラーです。{str(e)}")
         traceback.print_exc()
@@ -46,4 +78,6 @@ def create_text(messages: list, text2) -> str:
 
 if __name__ == "__main__":
     # load_model(model_file=model_file)
-    print(create_text([], text2="こんにちは！"))
+    # print(create_text([], text2="こんにちは！")
+    # join()
+    test()
