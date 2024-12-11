4f328bdc9b16b9cf0d91c8f9aa6b1a6d704a3424
kurocafe <aiue7298@gmail.com>
2024-12-11 22:06:37 +0900

    update: sbt

--- func/sbt/sbt.py
+++ func/sbt/sbt.py
@@ -1,21 +1,21 @@
 from style_bert_vits2.nlp import bert_models
 from style_bert_vits2.constants import Languages
 from style_bert_vits2.tts_model import TTSModel
-
 from torch.nn.utils.parametrizations import weight_norm
-
 from pathlib import Path
 from huggingface_hub import hf_hub_download
-
-# from IPython.display import Audio, display
 import soundfile as sf
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
+PATH = getenv("PATH")
 
-PATH = "./tmp/output.wav"
+FILE_PATH = f"{PATH}/tmp/output.wav"
 
-chara_dict = {0: {"name":"miyako", "model_file": "miyako/miyako_e100_s13400.safetensors"},
-              1: {"name":"tsukuyomi_test", "model_file": "tsukuyomi_test/tsukuyomi_test_e100_s10400.safetensors"},
-              2: {"name":"Yuuka", "model_file": "Yuuka/Yuuka_e100_s14400.safetensors"},
-              3: {"name":"Asuna", "model_file": "Asuna/Asuna_e100_s9700.safetensors"}
+chara_dict = {0: {"name":"miyako", "model_file": f"{PATH}/model_assets/miyako/miyako_e100_s13400.safetensors"},
+              1: {"name":"tsukuyomi_test", "model_file": f"{PATH}/model_assets/tsukuyomi_test/tsukuyomi_test_e100_s10400.safetensors"},
+              2: {"name":"Yuuka", "model_file": f"{PATH}/model_assets/Yuuka/Yuuka_e100_s14400.safetensors"},
+              3: {"name":"Asuna", "model_file": f"{PATH}/model_assets/Asuna/Asuna_e100_s9700.safetensors"}
 }
 
 model_dict = {
@@ -36,16 +36,13 @@ def load_models()-> None:
 #     hf_hub_download("litagin/style_bert_vits2_jvnv", file, local_dir="model_assets")
 def sbt2_voice(text :str, chara_id :int = 0)-> str:
     load_models()
-    # model_file = "tsukuyomi_test/tsukuyomi_test_e100_s10400.safetensors"
-    # config_file = "tsukuyomi_test/config.json"
-    # style_file = "tsukuyomi_test/style_vectors.npy"
     
     if chara_id not in model_dict:        
         chara = chara_dict[chara_id]
         name = chara["name"]
         model_file = chara["model_file"]
-        config_file = f"{name}/config.json"
-        style_file = f"{name}/style_vectors.npy"
+        config_file = f"{PATH}/model_assets/{name}/config.json"
+        style_file = f"{PATH}/model_assets/{name}/style_vectors.npy"
         
         assets_root = Path("model_assets")
 
@@ -60,10 +57,10 @@ def sbt2_voice(text :str, chara_id :int = 0)-> str:
     model = model_dict[chara_id]
 
     sr, audio = model.infer(text=text)
-    sf.write(PATH, audio, sr)
+    sf.write(FILE_PATH, audio, sr)
     # display(Audio(PATH))
     
-    return PATH
+    return FILE_PATH
 
 if __name__ == '__main__':
     sbt2_voice("録音テストを始めるよ")
